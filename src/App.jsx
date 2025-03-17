08d337834902a05d245dc6525f8786b89c3a4569
yashvi <yashvipalkhiwala2401@gmail.com>
2025-03-17 16:59:14 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './pages/Main'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        <Main/>
+    </h1>
+    </div>
+  )
+}
+
+export default App
